1cf7e96e8eb2cd0b54734cb6d2a7234fbbcedbb0
KalyanasishBera <Kalyan@gunjanappstudios.com>
2024-07-22 15:11:57 +0530

    push

---
+++ Assets/Script/UIController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+}
